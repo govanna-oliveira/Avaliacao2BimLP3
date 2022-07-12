dfc282b80e0a87311495eb1c1855a31e8d2878aa
geovanna-oliveira <102254889+govanna-oliveira@users.noreply.github.com>
2022-07-12 11:49:11 -0300

    Realização da prova

---
+++ Models/Student.cs
@@ -0,0 +1,21 @@
+namespace Avaliacao2BimLP3.Models;
+
+class Student
+{
+    public string Registration { get; set; }
+    public string Name { get; set; }
+    public string City { get; set; }
+    public bool Former { get; set; }
+
+    public Student(){
+        
+    }
+    
+    public Student(string registration, string name, string city)
+    {
+        Registration = registration;
+        Name = name;
+        City = city;
+        Former = false;
+    }
+}
